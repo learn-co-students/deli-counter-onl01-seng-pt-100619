f0dd304a8df92b7835a31f9ad5eef7bfe97ab211
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-17 03:44:52 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+require 'pry'
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    position_in_line = "The line is currently:"
+    katz_deli.each_with_index do |element, index|
+      position_in_line << "#{index} #{element}"
+
+    end
+  end
+
+end
+
+#def take_a_number(katz_deli, name)
