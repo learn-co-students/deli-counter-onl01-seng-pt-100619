e1c81efdf8fdeee03224da9ac6a38bdf1a8701be
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-10-23 14:15:05 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+# Write your code 
+def line(katz_deli)
+  str = "The line is currently: "
+  if katz_deli.length >= 1
+    katz_deli.each_with_index {|name, index| str.concat("#{index + 1}. " "#{name}")} 
+    puts str
+  else 
+  puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli.push(name)
+    puts "Welcome, " + "#{name}" +"." +" You are number "+ "#{katz_deli.length}" + " in line." 
+end
+
+  
